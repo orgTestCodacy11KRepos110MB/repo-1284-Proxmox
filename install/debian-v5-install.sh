26b66600d0ae8cd8a319c781c775e9ab6fadedd8
tteckster <tteckster@gmail.com>
2023-02-01 05:39:56 -0500

    Update debian-v5-install.sh

--- install/debian-v5-install.sh
+++ install/debian-v5-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-silent() { "$@" > /dev/null 2>&1; }
+silent() { command "$@" >/dev/null 2>&1; }
 if [ "$VERBOSE" == "yes" ]; then set -x; fi
 if [ "$DISABLEIPV6" == "yes" ]; then echo "net.ipv6.conf.all.disable_ipv6 = 1" >>/etc/sysctl.conf; $STD sysctl -p; fi
 YW=$(echo "\033[33m")
@@ -14,10 +14,8 @@ CM="${GN}✓${CL}"
 CROSS="${RD}✗${CL}"
 BFR="\\r\\033[K"
 HOLD="-"
-set -o errexit
+set -euo pipefail
 set -o errtrace
-set -o nounset
-set -o pipefail
 shopt -s expand_aliases
 alias die='EXIT=$? LINE=$LINENO error_exit'
 trap die ERR
@@ -46,14 +44,14 @@ function msg_error() {
 }
 
 msg_info "Setting up Container OS"
-sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
-locale-gen >/dev/null
-while [ "$(hostname -I)" = "" ]; do
-  echo 1>&2 -en "${CROSS}${RD} No Network! "
+sed -i "s/^#\($LANG\)/\1/" /etc/locale.gen
+locale-gen &> /dev/null
+until hostname -I; do
+  echo 1>&2 "${CROSS}${RD} No Network! "
   sleep $RETRY_EVERY
   ((NUM--))
   if [ $NUM -eq 0 ]; then
-    echo 1>&2 -e "${CROSS}${RD} No Network After $RETRY_NUM Tries${CL}"
+    echo 1>&2 "${CROSS}${RD} No Network After $RETRY_NUM Tries${CL}"
     exit 1
   fi
 done
@@ -72,14 +70,17 @@ if nc -zw1 8.8.8.8 443; then msg_ok "Internet Connected"; else
       exit 1
     fi
 fi
-RESOLVEDIP=$(nslookup "github.com" | awk -F':' '/^Address: / { matched = 1 } matched { print $2}' | xargs)
-if [[ -z "$RESOLVEDIP" ]]; then msg_error "DNS Lookup Failure"; else msg_ok "DNS Resolved github.com to $RESOLVEDIP"; fi
+if getent ahosts "github.com" >/dev/null 2>&1; then
+  msg_ok "DNS Resolved github.com"
+else
+  msg_error "DNS Lookup Failure"
+fi
 alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
 
 msg_info "Updating Container OS"
 $STD apt-get update
-$STD apt-get -y upgrade
+$STD apt-get -y full-upgrade
 msg_ok "Updated Container OS"
 
 msg_info "Installing Dependencies"
@@ -88,22 +89,25 @@ $STD apt-get install -y sudo
 $STD apt-get install -y mc
 msg_ok "Installed Dependencies"
 
-PASS=$(grep -w "root" /etc/shadow | cut -b6)
 echo "export TERM='xterm-256color'" >>/root/.bashrc
-if [[ $PASS != $ ]]; then
+root_shadow_entry=$(grep -w "root" /etc/shadow)
+if [ -n "$root_shadow_entry" ]; then
   msg_info "Customizing Container"
-  chmod -x /etc/update-motd.d/*
+  rm /etc/motd /etc/update-motd.d/10-uname 2>/dev/null
   touch ~/.hushlogin
-  GETTY_OVERRIDE="/etc/systemd/system/container-getty@1.service.d/override.conf"
-  mkdir -p $(dirname $GETTY_OVERRIDE)
-  cat <<EOF >$GETTY_OVERRIDE
+  getty_override_file="/etc/systemd/system/container-getty@1.service.d/override.conf"
+  mkdir -p "$(dirname $getty_override_file)"
+  cat <<EOF > "$getty_override_file"
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
 EOF
   systemctl daemon-reload
-  systemctl restart $(basename $(dirname $GETTY_OVERRIDE) | sed 's/\.d//')
+  systemctl restart $(basename "$(dirname "$getty_override_file")" | sed 's/\.d//')
   msg_ok "Customized Container"
+else
+  msg_error "Root shadow entry not found"
+  exit 1
 fi
 if [[ "${SSH_ROOT}" == "yes" ]]; then
   sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/g" /etc/ssh/sshd_config
